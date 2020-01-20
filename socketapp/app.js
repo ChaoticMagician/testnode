a04b14240079e205e11d7905a96f22d4a95503f6
ChaoticMagician <36400417+ChaoticMagician@users.noreply.github.com>
2020-01-20 17:41:28 +0800

    结构了解

--- socketapp/app.js
+++ socketapp/app.js
@@ -1,42 +1,32 @@
 let express = require('express');
 let app =express();
 let http = require('http').Server(app);
-let io = require('socket.io')(http);
+// let mysql = require('mysql');
 
-let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'Mysql1'
-})
-let sql = 'select * from user';
-connection.connect();
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'Mysql1'
+// })
+// let sql = 'select * from user';
+// connection.connect();
  
-connection.query(sql, function (error, results, fields) {
-  if (error) throw error;
-  console.log('The userTable is: ', results);
+// connection.query(sql, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The userTable is: ', results);
+// });
+app.use(express.static(__dirname + '/views'))
+app.get('/haha',function(req,res) {
+  res.send('haha.hello world')
 });
-// app.use(express.static(__dirname + '/views'))
-// app.get('/haha',function(req,res) {
-//   res.send('haha.hello world')
-// });
+app.get('/hello/:who?',function(req,res) {
+	if(req.params.who) {
+    	res.end("Hello, " + req.params.who + ".");
+	}
+    else {
+    	res.send("Hello, Guest.");
+	}
+});
 
-// io.on('connection',function(socket) {
-//   console.log(
-//     // socket,
-//     '这是socket'
-//     );
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//     io.emit('chat message', msg);
-//   });
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
-
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
+module.exports = app;
---
+++ socketapp/config/allConfig.js
@@ -0,0 +1,12 @@
+
+exports.serverInfo =  {
+  port:'3001',
+  view:'/views'
+};
+exports.databaseInfo={
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'Mysql1'
+};
+// module.exports = allconfig;
